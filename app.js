672ab522fe02a02fb3108bea6b43e3754d508c34
NhungTranPY <nhungtranpy91@gmail.com>
2023-04-18 15:20:14 +0700

    99. Finishing the Controllers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 // const expressHbs = require('express-handlebars')
+const errorController = require('./controllers/error')
 const app = express()
 
 // app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
@@ -19,9 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
-app.use((req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
-    res.status(404).render('404', {pageTitle: 'Page not Found'})
-})
+app.use(errorController.get404)
 
 app.listen(3000)
