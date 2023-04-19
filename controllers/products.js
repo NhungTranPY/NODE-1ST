420b7a0f985ac8b2a20cb7e182beb82120ee7ae1
NhungTranPY <nhungtranpy91@gmail.com>
2023-04-19 21:15:26 +0700

    102. Fetching Data from Files Via the Model

--- controllers/products.js
+++ controllers/products.js
@@ -21,13 +21,14 @@ exports.postAddProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
     // console.log('shop.js', adminData.products);
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
-    const products = Product.fetchAll()
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path:'/', 
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path:'/', 
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        })
     })
 }
